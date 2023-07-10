01e3710ff4380c278968b5c629374631dddae1a4
megaC <corloss@web.de>
2023-07-10 16:22:40 +0200

    updated UUID and PHY

--- src/ble.h
+++ src/ble.h
@@ -8,12 +8,13 @@
 #include <zephyr/bluetooth/uuid.h>
 #include <zephyr/logging/log.h>
 
-#define BT_UUID_OBS_SERVICE_VAL        BT_UUID_128_ENCODE(0x00001523, 0x1212, 0xefde, 0x1523, 0x785feabcd123)
+#define BT_UUID_OBS_SERVICE_VAL        BT_UUID_128_ENCODE(0x24749e30, 0xa637, 0x4fd1, 0x9040, 0x5a1b76932cf2)
 #define BT_UUID_OBS_SERVICE            BT_UUID_DECLARE_128(BT_UUID_OBS_SERVICE_VAL)
-#define BT_UUID_OBS_DISTANCES_CHRC_VAL BT_UUID_128_ENCODE(0x00001524, 0x1212, 0xefde, 0x1523, 0x785feabcd123)
+#define BT_UUID_OBS_DISTANCES_CHRC_VAL BT_UUID_128_ENCODE(0x24749e31, 0xa637, 0x4fd1, 0x9040, 0x5a1b76932cf2)
 #define BT_UUID_OBS_DISTANCES_CHRC     BT_UUID_DECLARE_128(BT_UUID_OBS_DISTANCES_CHRC_VAL)
 
 void ble_init(struct bt_conn_cb *bt_cb);
 void ble_send(struct bt_conn *ble_conn, int16_t *data_array, uint16_t data_array_length);
 
 #endif
+
